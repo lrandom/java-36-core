96c95f099fb70146cdba47475571c229a1f4ea7e
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-05-23 20:32:09 +0700

    dowwn img

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,14 +33,29 @@ public class Main {
             bufferedInputStream.close();
             fileInputStream.close();*/
 
-            FileReader fileReader = new FileReader("/Users/luan_prep_vn/Desktop/note.txt");
+            /*FileReader fileReader = new FileReader("/Users/luan_prep_vn/Desktop/note.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = "";
-            while ((line=bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
             fileReader.close();
-            bufferedReader.close();
+            bufferedReader.close();*/
+
+            String imagePath = "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/07/anh-dep-thien-nhien-2-1.jpg";
+            URL url = new URL(imagePath);
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(url.openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream("/Users/luan_prep_vn/Documents/anh-dep.jpg");
+            int i = 0;
+            while ((i = bufferedInputStream.read()) != -1) {
+                fileOutputStream.write(i);
+            }
+            fileOutputStream.flush();
+
+            bufferedInputStream.close();
+            fileOutputStream.close();
+
         } catch (Exception e) {
             System.out.println("File not found");
         }
