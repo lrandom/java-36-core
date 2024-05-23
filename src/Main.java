b45c097dc7434ec0795083778cb846bd32e6da6c
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-05-23 20:05:14 +0700

    buffer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,15 +9,37 @@ public class Main {
                 System.out.print((char) i);
             }
             fileInputStream.close();*/
+            /*var test = "C:\\Users\\luan_prep_vn\\Desktop\\test.jpeg";
+            System.out.println(test);
+            */
 
-            FileInputStream fileInputStream = new FileInputStream("/Users/luan_prep_vn/Desktop/test.jpeg");
+      /*      FileInputStream fileInputStream = new FileInputStream("/Users/luan_prep_vn/Desktop/test.jpeg");
             FileOutputStream fileOutputStream = new FileOutputStream("/Users/luan_prep_vn/Documents/test.jpeg");
             int i = 0;
             while ((i = fileInputStream.read()) != -1) {
                 fileOutputStream.write(i);
             }
             fileOutputStream.close();
-            fileInputStream.close();
+            fileInputStream.close();*/
+
+
+            /*FileInputStream fileInputStream = new FileInputStream("/Users/luan_prep_vn/Desktop/note.txt");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            int i = 0;
+            while ((i = bufferedInputStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+            bufferedInputStream.close();
+            fileInputStream.close();*/
+
+            FileReader fileReader = new FileReader("/Users/luan_prep_vn/Desktop/note.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while ((line=bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            fileReader.close();
+            bufferedReader.close();
         } catch (Exception e) {
             System.out.println("File not found");
         }
