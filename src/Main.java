669d84a0376c5c040f172780ee42506d559752a0
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-07-11 20:55:07 +0700

    update demo mysql

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-
-
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Thread t1 = new MyThread("My Thread 1");
-        Thread t2 = new MyThread("My Thread 2");
-        t2.setPriority(10);
-        t1.setPriority(1);
-        t1.start();
-        t2.start();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded successfully");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java36", "root",
+                    "koodinh@");
+            System.out.println("Connect success to DB");
+            System.out.println("Vui long nhap ten danh muc: ");
+            Scanner scanner = new Scanner(System.in);
+            String category = scanner.nextLine();
+
+            String sql = "INSERT INTO categories(name) VALUES('" + category + "')";
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
